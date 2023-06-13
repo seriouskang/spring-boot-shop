637987f9c26e7030336000f480039ba2eb22f861
seriouskang <scdean110@gmail.com>
2023-06-13 22:36:22 +0900

    project init

---
+++ src/main/java/spring/study/shop/SpringBootShopApplication.java
@@ -0,0 +1,13 @@
+package spring.study.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/study/shop/SpringBootShopApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.study.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
