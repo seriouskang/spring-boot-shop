78e734b39d587776227c1d38e6ba0b6c0d6e8eab
seriouskang <scdean110@gmail.com>
2023-06-14 23:27:35 +0900

    feat(GreetingController): 컨트롤러 신규

---
+++ src/main/java/spring/study/shop/presentation/GreetingController.java
@@ -0,0 +1,14 @@
+package spring.study.shop.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", "seriouskang");
+        return "greeting";
+    }
+}
